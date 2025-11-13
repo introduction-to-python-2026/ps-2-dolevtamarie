b2b6c4c22f56267d9fb592480a12c094637828b6
dolevtamarie <dolevtamarie@gmail.com>
2025-11-13 08:44:11 +0200

    Modify find_mean_std to return mean and sigma
    Update find_mean_std to return both mean and standard deviation.

--- math_utils.py
+++ math_utils.py
@@ -18,6 +18,7 @@ def find_mean(num1, num2, num3):
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    sigma = (((num1 - mean) ** 2 + (num2 - mean) ** 2 + (num3 - mean) ** 2) / 3) ** 0.5
-    return sigma
-    
+    sigma = (((num1 - mean) ** 2 +
+             (num2 - mean) ** 2 +
+             (num3 - mean) ** 2) / 3) ** 0.5
+    return mean, sigma
